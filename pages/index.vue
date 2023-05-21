5cc9466b700221c56de6ba2c16055071389a8845
zhaogongchengsi <zzh1586169624@163.com>
2023-05-21 15:52:11 +0800

    init

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,7 @@ const online = useOnline()
 
 <template>
   <div>
-    <Logos mb-6 />
-    <Suspense>
-      <ClientOnly>
-        <PageView v-if="online" />
-        <div v-else text-gray:80>
-          You're offline
-        </div>
-      </ClientOnly>
-      <template #fallback>
-        <div italic op50>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
+    <!-- <Logos mb-6 /> -->
+    resume
   </div>
 </template>
