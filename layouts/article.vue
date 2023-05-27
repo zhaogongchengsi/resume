94328c3eee238cf518ed1c1939e5fbf90b27835a
zhaogongchengsi <zzh1586169624@163.com>
2023-05-27 17:14:53 +0800

    feat: 控制文章aside 消失出现

--- layouts/article.vue
+++ layouts/article.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useWindowSize } from '@vueuse/core'
 import Header from '~/components/Header.vue'
 
+const isShow = ref(false)
+const { width } = useWindowSize()
+
+const asideShow = computed(() => {
+  if (width.value > 780)
+    return true
+
+  return isShow.value
+})
+
 function onMenuHandler() {
+  isShow.value = !isShow.value
 }
 </script>
 
@@ -17,12 +30,27 @@ function onMenuHandler() {
       </template>
     </Header>
   </header>
-  <main class="mx-auto flex flex-1 justify-center resume-container">
+  <main class="mx-auto flex flex-1 justify-center resume-container 2xl:gap-8 lg:gap-4 md:gap-2 xl:gap-6">
     <article class="flex-1">
       <slot />
     </article>
-    <aside class="w-70">
-      Aside
+    <aside class="fixed left-0 top-0 hidden h-screen w-full bg-gray-900/70 md:static md:w-70 md:bg-transparent" :class="{ 'block!': asideShow }" @click.self="isShow = !isShow">
+      <Transition>
+        <nav v-show="asideShow" class="px-3 py-10 md:p-0">
+          <ContentNavigation v-slot="{ navigation }">
+            <ul>
+              <li v-for="link of navigation" :key="link._path">
+                <NuxtLink :to="link._path">
+                  {{ link.title }}
+                </NuxtLink>
+              </li>
+            </ul>
+          </ContentNavigation>
+        </nav>
+      </Transition>
     </aside>
   </main>
 </template>
+
+<style>
+</style>
