d924db7b2118a917a0ef0dc0d2f6e27ec35ab297
zhaogongchengsi <zzh1586169624@163.com>
2023-06-02 15:11:42 +0800

    feat: loading

--- views/Repositories.vue
+++ views/Repositories.vue
@@ -6,17 +6,9 @@ const { data } = await useFetch('/api/repositories')
   <ClientOnly>
     <RepoSwiper :data="data" />
     <template #fallback>
-      <section class="grid my-4 lg:my-8 md:my-6 xl:my-10">
-        <div v-for="(item) in data" :key="item.repository">
-          <a :href="item.link" target="_blank">
-            <h3>{{ item.repository }}</h3>
-            <p>{{ item.description }}</p>
-            <span>{{ item.language }}</span>
-          </a>
-        </div>
+      <section class="my-4 h-40 flex items-center justify-center lg:my-8 md:my-6 xl:my-10">
+        <div> Loading... </div>
       </section>
     </template>
   </ClientOnly>
 </template>
-
-<style lang='scss'></style>
