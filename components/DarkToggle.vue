e44e8fbe4b074d4a06501ded1ddbb669d42237bf
zhaogongchengsi <zzh1586169624@163.com>
2023-07-12 16:43:09 +0800

    feat: 新增主题切换动画

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable @typescript-eslint/ban-ts-comment -->
 <script setup lang="ts">
 const color = useColorMode()
 
@@ -9,13 +10,61 @@ useHead({
   }],
 })
 
-function toggleDark() {
+const isDark = ref(color.value === 'dark')
+
+watch(isDark, () => {
   color.preference = color.value === 'dark' ? 'light' : 'dark'
+})
+
+function toggleDark(event: MouseEvent) {
+  // @ts-expect-error
+  const isAppearanceTransition = document.startViewTransition
+    && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+  if (!isAppearanceTransition) {
+    isDark.value = !isDark.value
+    return
+  }
+
+  const x = event.clientX
+  const y = event.clientY
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  )
+
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  })
+
+  transition.ready
+    .then(() => {
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ]
+      document.documentElement.animate(
+        {
+          clipPath: isDark.value
+            ? [...clipPath].reverse()
+            : clipPath,
+        },
+        {
+          duration: 400,
+          easing: 'ease-out',
+          pseudoElement: isDark.value
+            ? '::view-transition-old(root)'
+            : '::view-transition-new(root)',
+        },
+      )
+    })
 }
 </script>
 
 <template>
-  <button class="!outline-none" @click="toggleDark">
+  <button class="!outline-none" @click="toggleDark($event)">
     <div class="i-carbon-sun dark:i-carbon-moon icon-default!" />
   </button>
 </template>
