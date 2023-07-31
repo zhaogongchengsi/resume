6c31f1521a3c7c68777a3e0531e49857e1a4e648
zhaogongchengsi <zzh1586169624@163.com>
2023-07-31 17:20:09 +0800

    change: 修改文章渲染

--- pages/article/[name].vue
+++ pages/article/[name].vue
@@ -1,13 +1,37 @@
 <script setup lang='ts'>
-const route = useRoute()
-
 definePageMeta({
   layout: 'article',
+})
+
+const router = useRoute()
+const { locale } = useI18n()
+
+const articlePath = computed(() => {
+  return locale.value === 'en' ? router.params.name : `${router.params.name}_${locale.value}`
 })
 </script>
 
 <template>
-  <div class="resume-prose mx-auto max-w-180 w-full rounded-lg bg-white/40 p-4 backdrop-blur-sm dark:bg-black/40">
-    <ContentDoc path="crypto" />
+  <div class="mx-auto max-w-180 w-full rounded-lg bg-white/40 p-4 backdrop-blur-sm dark:bg-black/40">
+    <ContentDoc :path="articlePath">
+      <template #default="{ doc }">
+        <h3 class="resume-prose-ignore mt-3 text-center text-6 md:text-7">
+          {{ doc.title }}
+        </h3>
+        <div class="mb-8 mt-3 flex items-center justify-center gap-10 opacity-50">
+          <span>{{ doc.author }}</span>
+          <span>{{ doc.time }}</span>
+        </div>
+        <article class="resume-prose">
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+      <template #not-found>
+        <div class="flex flex-col items-center gap-10 py-10">
+          <div h-7 w-7 md:h-10 md:w-10 class="i-tabler-info-square-rounded" />
+          <h6>{{ locale === "en" ? "英文" : "中文" }} {{ $t("not-exist") }}</h6>
+        </div>
+      </template>
+    </ContentDoc>
   </div>
 </template>
