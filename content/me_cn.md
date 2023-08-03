d1e3b1c257a4524ef4b44e8489793ff2dae6b976
zhaogongchengsi <zzh1586169624@163.com>
2023-08-03 17:42:35 +0800

    feat: 添加主页卡片

--- content/me_cn.md
+++ content/me_cn.md
@@ -15,6 +15,22 @@ head:
       content: 'Zhaozunhong'
     - name: 'copyright'
       content: '© 2023 zhaogongchengsi'
+projects:
+  - project:
+      icon: '👑'
+      name: 'resume'
+      details: 'A web app to showcase my resume and projects.'
+      github: 'https://github.com/zhaogongchengsi/resume'
+  - project:
+      icon: '✨'
+      name: 'starter-vue'
+      details: 'vue 3 starter template'
+      github: 'https://github.com/zhaogongchengsi/starter-vue'
+  - project:
+      icon: '🎉'
+      name: 'starter-gin'
+      details: 'gin go web framework starter template'
+      github: 'https://github.com/zhaogongchengsi/starter-gin'
 ---
 
 你好，我的一个主业是前端开发工程师，主要职业是前端开发内容。所以常使用的语言是 **JS(TS)**, 在此之上我会使用**Vue**和**React**等框架，但是由于他们对于SEO不够友好，我还会使用Nuxt和Next。我称这为前端的后端。虽然主业是前端开发，但我业余时间，我还会研究一些其他的语言，比如，**Golang**，**Rust**，以及一些其他的开发技术，比如**Three.js**，让我能实现3D场景的开发。所以我觉得我更像是一位Web开发程序员，所有与Web开发的技术，我都有兴趣研究。并且乐此不疲。但我除了前端开发之外，我还会去健身房健身。并且身体才是革命的本钱。
--- pages/index.vue
+++ pages/index.vue
@@ -21,11 +21,16 @@ const me = computed(() => {
             <h1 class="text-8 font-bold md:text-16 sm:text-12">
               {{ doc.title }}
             </h1>
-            <p class="from-blue-500 to-purple-500 bg-gradient-to-r bg-clip-text text-6 font-semibold text-transparent sm:text-8">
+            <p
+              class="from-blue-500 to-purple-500 bg-gradient-to-r bg-clip-text text-6 font-semibold text-transparent sm:text-8"
+            >
               {{ doc.description }}
             </p>
 
-            <router-link to="/article" class="flex items-center gap-5 border border-2 border-indigo-500/75 rounded-lg px-4 py-2 shadow-indigo-500/70 hover:border-indigo-500 md:rounded-xl md:px-5 md:py-3 hover:shadow-lg">
+            <router-link
+              to="/article"
+              class="flex items-center gap-5 border border-2 border-indigo-500/75 rounded-lg px-4 py-2 shadow-indigo-500/70 hover:border-indigo-500 md:rounded-xl md:px-5 md:py-3 hover:shadow-lg"
+            >
               <span class="text-3 md:text-4">{{ $t("view") }}</span>
               <div h-5 w-5 text-indigo class="i-carbon:plan" />
             </router-link>
@@ -39,7 +44,48 @@ const me = computed(() => {
         <article class="home-prose">
           <ContentRenderer :value="doc" />
         </article>
+
+        <ul class="grid grid-cols-1 gap-3 md:grid-cols-3 sm:grid-cols-2 md:gap-5 sm:gap-4">
+          <li
+            v-for="(project, index) of doc.projects" :key="index"
+            class="group relative flex overflow-hidden border border-black/30 rounded-md p-1px dark:border-white/30"
+          >
+            <div class="gradient-border-light absolute left-0 top-0 z-9 hidden h-full w-full animate-spin ease-linear light:group-hover:block" />
+            <div class="gradient-border-dark absolute left-0 top-0 z-9 hidden h-full w-full animate-spin ease-linear dark:group-hover:block" />
+
+            <a href="#" class="z-10 h-full w-full flex flex-col gap-3 rounded-md bg-[var(--backgroud)] p-3">
+              <div class="flex items-center gap-2 sm:gap-3">
+                <span class="text-5 md:text-7 sm:text-6">{{ project.icon }}</span>
+                <span class="text-5 sm:text-6">{{ project.name }}</span>
+              </div>
+              <p class="text-4 text-white/50 sm:text-5">{{ project.details }}</p>
+            </a>
+          </li>
+        </ul>
       </ContentDoc>
     </div>
   </div>
 </template>
+
+<style>
+.gradient-border-dark {
+  background: linear-gradient(360deg, hsla(0, 0%, 100%, .5), hsla(0, 0%, 100%, .1), #fff, hsla(0, 0%, 100%, .3));
+}
+
+.gradient-border-light {
+  background: linear-gradient(360deg, #00dc82, #fff, rgba(54, 228, 218, .7), rgba(29, 224, 177, .3));
+}
+
+.animate-spin {
+  animation:  spin 2s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
