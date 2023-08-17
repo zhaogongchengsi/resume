53ee8fbd0dc202aca13afab8937c023b4efc351c
zhaogongchengsi <zzh1586169624@163.com>
2023-08-17 10:15:33 +0800

    feat: 列表样式完善

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -11,9 +11,34 @@ const others = computed(() => {
 
 <template>
   <div class="flex flex-col items-center gap-10 px-3 py-5 md:my-8 md:px-0">
-    <h3>Starter Templates</h3>
-    <project-list :data="templates" />
-    <h3>Others</h3>
-    <project-list :data="others" />
+    <div class="project-card">
+      <h3 class="project-title">
+        Starter Templates
+      </h3>
+      <project-list :data="templates" />
+    </div>
+    <div class="project-card">
+      <h3 class="project-title">
+        Others
+      </h3>
+      <project-list :data="others" />
+    </div>
   </div>
 </template>
+
+<style>
+.project-card {
+  width: 100%;
+  position: relative;
+  @apply flex flex-col items-center;
+}
+
+.project-title {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  @apply text-5 md:text-8 opacity-10 font-bold;
+  z-index: 1;
+}
+</style>
--- views/project-list.vue
+++ views/project-list.vue
@@ -13,20 +13,19 @@ const props = defineProps({
 </script>
 
 <template>
-  <ul class="max-w-250 w-full">
+  <ul class="z-2 max-w-200 w-full">
     <li v-for="(item, index) of props.data" :key="index" class="my-3 md:my-5 md:my-7">
       <a :href="item.github" class="group flex gap-2 rounded-md bg-gray-400/10 px-3 py-4 hover:bg-gray-400/20">
         <div v-if="item.icon" class="h-8 w-8 flex items-center justify-center opacity-25 group-hover:opacity-40">
           <div class="i-carbon:logo-github h-5 w-5" />
         </div>
         <div class="flex-1">
-          <span class="block text-5 text-neutral-300/80 group-hover:text-neutral-300">{{ item.name }}</span>
-          <p class="my-2 text-3 text-neutral-400/70 group-hover:text-neutral-400">{{ item.details }}</p>
+          <span class="block text-5 font-semibold text-neutral-300/80 md:text-6 group-hover:text-neutral-300">{{ item.name }}</span>
+          <p class="my-2 text-3 font-light text-neutral-400/70 md:text-4 group-hover:text-neutral-400">{{ item.details }}</p>
           <div v-if="item.language" class="flex justify-end gap-2">
             <span v-for="(language, i) of item.language" :key="i" class="text-2 opacity-15 group-hover:opacity-20">{{ language.toLocaleLowerCase() }}</span>
           </div>
         </div>
-
       </a>
     </li>
   </ul>
