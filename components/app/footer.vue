1a67d64c38a465d18b722f90726a063141c7c7c6
zhaogongchengsi <zzh1586169624@163.com>
2023-12-04 13:24:40 +0800

    chore: 添加 footer

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -4,8 +4,10 @@
 
 <template>
   <footer class="app-footer-container">
-    <div class="area-x mx-auto h-full flex items-center container">
-      footer
+    <div class="mx-auto h-full flex items-center justify-between area-x container">
+      <p class="text-sm text-gray-400">
+        <a target="_blank" class="text-gray-400 hover:text-gray-500 hover:underline" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> © 2022年至今 zhaozunhong
+      </p>
     </div>
   </footer>
 </template>
