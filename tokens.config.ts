4279c4e584bcbb8fe54b8cccdec8c3a099f72e51
zhaogongchengsi <zzh1586169624@163.com>
2023-12-04 13:38:24 +0800

    chore: 修改样式

--- tokens.config.ts
+++ tokens.config.ts
@@ -178,10 +178,10 @@ export default defineTheme({
       letterSpacing: '0.01em',
       backgroundColor: {
         initial: '#f6f8fa',
-        dark: '#1c1c1f',
+        dark: '#313131',
       },
       padding: '0.25rem 0.5rem',
-      borderRadius: '3px',
+      borderRadius: '5px',
     },
     table: {
       align: 'center',
