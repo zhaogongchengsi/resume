46a36a6c4ddaa71e8a31ec23b1a88d5a684891b0
zhaogongchengsi <zzh1586169624@163.com>
2023-12-06 16:12:11 +0800

    chore: 啥也没干

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: ['zhaozunhong', 'Resume', 'ZZH Resume', 'blog', 'zzh blog'].join(' ') },
+        { name: 'description', content: ['zhaozunhong', 'Resume', 'ZZH Resume', 'blog', 'zzh blog', 'Vue', 'React', 'Web Development', 'open source development'].join(' ') },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
       ],
     },
--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 运行 pnpm generate 命令
-pnpm generate
-
-# 复制并覆盖 .output/public 目录到 /home/blog
-cp -Rf .output/public /home/blog
